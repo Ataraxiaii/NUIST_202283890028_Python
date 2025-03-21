a7de2aa8f02787afff550a1e047f36f46d563337
Xuanru Guo <2722458837@qq.com>
2025-03-21 16:27:23 +0000

    commit MultiplyThreeNums.py

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,15 @@
+# Calculate the product of three numbers
+# Author: Xuanru Guo
+# use product
+
+# enter three numbers
+num1 = float(input("Please enter the first number: "))
+num2 = float(input("Please enter the second number: "))
+num3 = float(input("Please enter the third number: "))
+
+# calculate the product
+product = num1 * num2 * num3
+
+# print the result
+print(f"The product of {num1}, {num2} and {num3} is {product}.")
+
